ca6fd647cec35389602bb76065b6400e1e757d08
William Skiles <williamskiles@Williams-MacBook-Air.local>
2025-11-20 11:00:07 -0600

    Fix Next.js config for Vercel (use next.config.mjs)

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // You can add other config options here if you ever need them.
+};
+
+export default nextConfig;
